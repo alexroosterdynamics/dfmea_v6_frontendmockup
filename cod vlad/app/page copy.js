20318c00f7ccac434fd8d76b44ab9234b8b1fb77
alexroosterdynamics <alex@roosterdynamics.com>
2026-01-16 15:19:04 +0200

    s

--- cod vlad/app/page copy.js
+++
@@ -1,5 +0,0 @@
-import ProcessCanvas from './components/ProcessCanvas';
-
-export default function Home() {
-  return <ProcessCanvas />;
-}
--- cod vlad/app/page.js
+++
@@ -1,6 +0,0 @@
-// app/process/page.jsx
-import ProcessFlow from '@/app/components/ProcessFlow';
-
-export default function Page() {
-  return <ProcessFlow />;
-}
